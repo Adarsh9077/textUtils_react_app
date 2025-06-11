dff49d425d28828cb8b7633f61baafd89ba58a5a
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-06-12 00:29:39 +0530

    working on text form component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import propTypes from "prop-types";
 
 export default function TextForm(props) {
+  const handleUpperCaseClick = () => {
+    console.log("object");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const onChange = (event) => {
+    console.log("object");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here");
   return (
     <>
       <div className="container my-3">
         <h2>{props.heading}</h2>
         <div className="mb-3">
-          <textarea className="form-control" id="myBox" rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            onChange={onChange}
+          ></textarea>
         </div>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={handleUpperCaseClick}
+        >
+          Convert to UpperCase
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
       </div>
     </>
   );
 }
 
 TextForm.propTypes = { heading: propTypes.string };
+// 04:00 part 8
