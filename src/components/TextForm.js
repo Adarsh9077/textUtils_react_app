49d0f7da858d57d6000374da526bf7e7838b87d4
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-06-13 00:52:40 +0530

    Work on new features

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,10 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handleLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
   const onChange = (event) => {
     console.log("object");
     setText(event.target.value);
@@ -32,16 +36,26 @@ export default function TextForm(props) {
         >
           Convert to UpperCase
         </button>
+        <button
+          className="btn btn-primary mx-3"
+          type="button"
+          onClick={handleLowerCase}
+        >
+          Convert to LowerCase
+        </button>
       </div>
       <div className="container my-3">
-        <h1>Your text summary</h1>
+        <h2>Your text summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
       </div>
     </>
   );
 }
 
 TextForm.propTypes = { heading: propTypes.string };
-// 04:00 part 8
+// 00:00 part 9
